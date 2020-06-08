1c70d7a814e550198747b84713d3ed85fc465c41
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-06-08 20:44:45 -0300

    Building the initial discount-calculator structure.

---
+++ discount-calculator-service/src/main/java/com/fredoliveira/discountcalculator/Application.java
@@ -0,0 +1,13 @@
+package com.fredoliveira.discountcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
