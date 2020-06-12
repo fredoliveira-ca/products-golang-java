c9bad70410262797799148a97df3959f737c6951
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-06-12 19:57:27 -0300

    Mapping user domain to proto.

---
+++ user-service/data/mapper/userMapper.go
@@ -0,0 +1,19 @@
+package mapper
+
+import (
+	"github.com/fredoliveira-ca/products-golang-java/user-service/app/grpc/proto/userpb"
+	"github.com/fredoliveira-ca/products-golang-java/user-service/domain"
+)
+
+// ToProto is ...
+func ToProto(user domain.User) *userpb.UserProto {
+	return &userpb.UserProto{
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		DateOfBirth: &userpb.Date{
+			Year:  int32(user.DateOfBirth.Year()),
+			Month: int32(user.DateOfBirth.Month()),
+			Day:   int32(user.DateOfBirth.Day()),
+		},
+	}
+}
