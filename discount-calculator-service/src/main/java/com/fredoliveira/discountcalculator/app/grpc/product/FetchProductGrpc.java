3cb2e57a2a866c2b50125ae19a09925412bc2df5
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-06-12 22:41:02 -0300

    Implementing gRPC client to fetch a product information.

---
+++ discount-calculator-service/src/main/java/com/fredoliveira/discountcalculator/app/grpc/product/FetchProductGrpc.java
@@ -0,0 +1,35 @@
+package com.fredoliveira.discountcalculator.app.grpc.product;
+
+import com.fredoliveira.discountcalculator.domain.Product;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.product.ProductPriceRequest;
+import io.grpc.product.ProductPriceResponse;
+import io.grpc.product.ProductPriceServiceGrpc;
+
+public class FetchProductGrpc {
+    public static final String ADDRESS = "localhost";
+    public static final int PORT = 50051;
+
+    public Product fetchBy(String productId) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(ADDRESS, PORT)
+                .usePlaintext()
+                .build();
+
+        ProductPriceServiceGrpc.ProductPriceServiceBlockingStub stub
+                = ProductPriceServiceGrpc.newBlockingStub(channel);
+
+        ProductPriceResponse response = stub.fetchOne(
+                ProductPriceRequest.newBuilder()
+                        .setProductId(productId)
+                        .build()
+        );
+
+        channel.shutdown();
+
+        return Product.builder()
+                .id(productId)
+                .priceInCents(response.getValueInCents())
+                .build();
+    }
+}
