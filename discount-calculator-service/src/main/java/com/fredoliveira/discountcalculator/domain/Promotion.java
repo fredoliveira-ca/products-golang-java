35a1d6c376d32f0f2e90e9f0e5f1c3bd60167bf5
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-06-12 22:42:38 -0300

    Adding promotion class to handle available promotions.

---
+++ discount-calculator-service/src/main/java/com/fredoliveira/discountcalculator/domain/Promotion.java
@@ -0,0 +1,29 @@
+package com.fredoliveira.discountcalculator.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.Month;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static java.time.LocalDate.now;
+import static java.time.Month.JUNE;
+
+@Getter
+@AllArgsConstructor
+public enum Promotion {
+
+    BIRTHDAY("desc", BigDecimal.valueOf(0.05), null, null),
+    BLACK_FRIDAY("desc", BigDecimal.valueOf(0.10), JUNE, 11);
+
+    private final String description;
+    private final BigDecimal discount;
+    private final Month month;
+    private final Integer day;
+
+    public LocalDate getPromotionDate() {
+        return LocalDate.of(now().getYear(), this.getMonth(), this.getDay());
+    }
+
+}
