7fc53d16d0379bde0dec303330fb1e1d772a9c16
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-06-12 22:49:28 -0300

    Adding available discounts.

---
+++ discount-calculator-service/src/main/java/com/fredoliveira/discountcalculator/data/BirthdayDiscount.java
@@ -0,0 +1,40 @@
+package com.fredoliveira.discountcalculator.data;
+
+import java.math.BigDecimal;
+
+import com.fredoliveira.discountcalculator.app.grpc.user.FetchUserGrpc;
+import com.fredoliveira.discountcalculator.app.service.DiscountFinder;
+
+import static java.math.BigDecimal.valueOf;
+import static java.time.LocalDate.now;
+
+public class BirthdayDiscount implements DiscountFinder {
+
+    private static final java.util.logging.Logger log = java.util.logging.Logger.getLogger(BirthdayDiscount.class.getName());
+    private static final BigDecimal BIRTHDAY_DISCOUNT = valueOf(0.10);
+
+    private final String userId;
+
+    public BirthdayDiscount(String userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public BigDecimal getDiscount() {
+        if(isUserBirthDay()) {
+            return BIRTHDAY_DISCOUNT;
+        }
+
+        return BigDecimal.ZERO;
+    }
+
+    private boolean isUserBirthDay() {
+        try {
+            return now().isEqual(new FetchUserGrpc().fetchBy(userId).getDateOfBirth());
+        } catch (Exception e) {
+            log.info("gRPC user server is down.");
+            return false;
+        }
+    }
+
+}
---
+++ discount-calculator-service/src/main/java/com/fredoliveira/discountcalculator/data/BlackFridayDiscount.java
@@ -0,0 +1,26 @@
+package com.fredoliveira.discountcalculator.data;
+
+import java.math.BigDecimal;
+
+import com.fredoliveira.discountcalculator.app.service.DiscountFinder;
+
+import static com.fredoliveira.discountcalculator.domain.Promotion.BLACK_FRIDAY;
+import static java.math.BigDecimal.valueOf;
+import static java.time.LocalDate.now;
+
+public class BlackFridayDiscount implements DiscountFinder {
+    public static final BigDecimal BLACK_FRIDAY_DISCOUNT = valueOf(0.10);
+
+    @Override
+    public BigDecimal getDiscount() {
+        if(isBlackFriday()) {
+            return BLACK_FRIDAY_DISCOUNT;
+        }
+        return BigDecimal.ZERO;
+    }
+
+    private boolean isBlackFriday() {
+        return now().isEqual(BLACK_FRIDAY.getPromotionDate());
+    }
+
+}
