3d8184cfe7b1cf402fa39967b80703e39fefaf62
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-06-12 22:56:17 -0300

    Implementing discount calculator which will search the product value and deal with available discounts.

---
+++ discount-calculator-service/src/main/java/com/fredoliveira/discountcalculator/app/service/DiscountService.java
@@ -0,0 +1,28 @@
+package com.fredoliveira.discountcalculator.app.service;
+
+import com.fredoliveira.discountcalculator.app.grpc.product.FetchProductGrpc;
+import com.fredoliveira.discountcalculator.domain.Discount;
+import io.grpc.product.DiscountRequest;
+import io.grpc.product.DiscountResponse;
+import io.grpc.product.DiscountServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class DiscountService extends DiscountServiceGrpc.DiscountServiceImplBase {
+
+    @Override
+    public void calculate(DiscountRequest request, StreamObserver<DiscountResponse> responseObserver) {
+        final var product = new FetchProductGrpc().fetchBy(request.getProductId());
+        final var discount = new DiscountCalculator().calculateDiscount(product.getPriceInCents(), request.getUserId());
+        respond(responseObserver, discount);
+    }
+
+    private void respond(StreamObserver<DiscountResponse> responseObserver, Discount discount) {
+        DiscountResponse response = DiscountResponse.newBuilder()
+                .setPct(discount.getPercentage().floatValue())
+                .setValueInCents(discount.getValueInCents())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
